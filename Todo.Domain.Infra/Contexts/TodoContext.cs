5de64befde3c35b927715cddc0aa369138949ac5
Yan Bruno Silva Santos <47213415+YanBruno@users.noreply.github.com>
2020-12-01 07:56:28 -0300

    Update TodoContext.cs
    Escrita com menos código.

--- Todo.Domain.Infra/Contexts/TodoContext.cs
+++ Todo.Domain.Infra/Contexts/TodoContext.cs
@@ -14,13 +14,15 @@ namespace Todo.Domain.Infra.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TodoItem>().ToTable("Todo");
-            modelBuilder.Entity<TodoItem>().Property(x => x.Id);
-            modelBuilder.Entity<TodoItem>().Property(x => x.User).HasMaxLength(120).HasColumnType("varchar(120)");
-            modelBuilder.Entity<TodoItem>().Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
-            modelBuilder.Entity<TodoItem>().Property(x => x.Done).HasColumnType("bit");
-            modelBuilder.Entity<TodoItem>().Property(x => x.Date);
-            modelBuilder.Entity<TodoItem>().HasIndex(b => b.User);
+            modelBuilder.Entity<TodoItem>(todo => {
+                todo.ToTable("Todo");
+                todo.Property(x => x.Id);
+                todo.Property(x => x.User).HasMaxLength(120).HasColumnType("varchar(120)");
+                todo.Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+                todo.Property(x => x.Done).HasColumnType("bit");
+                todo.Property(x => x.Date);
+                todo.HasIndex(b => b.User);
+            });
         }
     }
 }
